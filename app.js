91ec4fbdc10c016a31c5aab5a36b8c09a3d0e901
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-13 06:18:23 -0700

    weather app

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-axios('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=MPSJFVSK5GBJUECH7TS82XAVL')
-.then((res)=>{
+
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    axios(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input.value}?key=MPSJFVSK5GBJUECH7TS82XAVL
+`)
+   .then((res)=>{
     console.log(res.data)
-}).catch((error)=>{
-    
+    })
+   .catch((error)=>{
+    console.log(error)
+    })
 })
+
+
+
