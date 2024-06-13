53abde549b66e5d436f94379e9cdba637b017d95
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-13 06:51:13 -0700

    weather app

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ form.addEventListener('submit', (e)=>{
     <h1>${res.data.address}</h1>
     <h2>Latitude : ${res.data.latitude}</h2>
     <h2>Longitude : ${res.data.longitude}</h2>
-    <h3>${res.data.currentConditions.cloudcover}</h3>
-    <h3>${res.data.currentConditions.conditions}</h3>
-    <h3>${res.data.currentConditions.datetime}</h3>
+    <h3>Cloudcover : ${res.data.currentConditions.cloudcover}</h3>
+    <h3>Conditions : ${res.data.currentConditions.conditions}</h3>
+    <h3>Datetime : ${res.data.currentConditions.datetime}</h3>
     <h3>${res.data.currentConditions.icon}</h3>
     `
     })
