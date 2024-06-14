4afbdf14cb0d44f34870276f431b7579a84ec9a0
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-14 07:56:26 -0700

    weather app

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
-const input = document.querySelector('input');
-const form = document.querySelector('form');
+// const input = document.querySelector('input');
+// const form = document.querySelector('form');
 
-const div = document.querySelector('div')
+// const div = document.querySelector('div')
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    axios(`http://api.weatherapi.com/v1/current.json?key=b90421cd7596432bbb2144327241406&q=${input.value}&aqi=no`)
-   .then((res)=>{
-    div.innerHTML = ''
-    console.log(res.data)
-    div.innerHTML += `
-    <h1>${res.data.location.name}</h1>
-    `
-    })
-   .catch((error)=>{
-    console.log(error)
-    })
-})
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+//     axios(`http://api.weatherapi.com/v1/current.json?key=b90421cd7596432bbb2144327241406&q=${input.value}&aqi=no`)
+//    .then((res)=>{
+//     div.innerHTML = ''
+//     console.log(res.data)
+//     div.innerHTML += `
+//     <h1>${res.data.location.name}</h1>
+//     `
+//     })
+//    .catch((error)=>{
+//     console.log(error)
+//     })
+// })
 
 
 
