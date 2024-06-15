4f98bbf794547c3343d3a623fb1268f7d3e47393
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-15 02:55:42 -0700

    weather app

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ form.addEventListener('submit', (e)=>{
     div.innerHTML = ''
     console.log(res.data)
     div.innerHTML += `
-        <div class="card text-center" style="width: 18rem;">
+        <div class="card text-center form-cards-js text-dark" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">${res.data.location.name}</h5>
               <h6 class="card-subtitle mb-2 text-body-secondary">${res.data.location.localtime} , ${res.data.location.country} , ${res.data.location.region}</h6>
+              <h3 class="card-title mt-3">${res.data.current.condition.text}</h3>
               <img width="160px" src=${res.data.current.condition.icon} alt='weatherImg' />
-              <h3 class="card-title">${res.data.current.temp_c} <sup>o</sup>C</h3>
+              <h3 class="card-title text-white">${res.data.current.temp_c} <sup>o</sup>C</h3>
             </div>
           </div>
     `
